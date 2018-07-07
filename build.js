156c4a59dacb970a6c9d37d4f9874ea7b551c7ef
ngsiolei <ngsiolei@gmail.com>
2018-07-07 16:04:00 +0800

    format code

--- build.js
+++ build.js
@@ -1,25 +1,27 @@
-var fs = require('fs');
-var entries = [];
-var entryLen = 0;
-var out;
+'use strict';
 
-fs.readdir('./', function (err, files) {
+const fs = require('fs');
+let entries = [];
+let entryLen = 0;
+let out;
+
+fs.readdir('./', function(err, files) {
   if (err) {
     throw err;
     return;
   }
-  files.forEach(function (x) {
+  files.forEach(function(x) {
     if (x.match(/^partial-[0-9]+\.json/) !== null) {
       entries.push(x);
     }
   });
-  entries.sort(function (a, b) {
-    var aNum = parseInt(a.replace('partial-', ''), 10);
-    var bNum = parseInt(b.replace('partial-', ''), 10);
+  entries.sort(function(a, b) {
+    const aNum = parseInt(a.replace('partial-', ''), 10);
+    const bNum = parseInt(b.replace('partial-', ''), 10);
     return aNum - bNum;
   });
   entryLen = entries.length;
-  read(0, function () {
+  read(0, function() {
     console.log(JSON.stringify(out, null, 2));
   });
 });
@@ -29,15 +31,15 @@ function read(i, cb) {
     cb();
     return;
   }
-  var readStream = fs.createReadStream(entries[i], {'encoding': 'utf8'});
-  var content = ''; 
-  readStream.on('data', function (chunk) {
+  const readStream = fs.createReadStream(entries[i], {encoding: 'utf8'});
+  let content = '';
+  readStream.on('data', function(chunk) {
     content += chunk;
   });
-  readStream.on('end', function () {
-    var obj = JSON.parse(content);
+  readStream.on('end', function() {
+    const obj = JSON.parse(content);
     if (out && typeof out === 'object') {
-      var partialGeo = obj.features[0].geometry.coordinates[0];
+      const partialGeo = obj.features[0].geometry.coordinates[0];
       out.features[0].geometry.coordinates.push(partialGeo);
     } else {
       out = obj;
